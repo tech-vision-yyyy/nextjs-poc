4c95dfe4ae23b1fe44e05e8bd9d159281f7277c5
Andy Bettisworth <allen.a.bettisworth@accenture.com>
2021-09-08 08:20:40 -0500

    rm comments

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,9 @@ import { useRouter } from "next/router";
 import { Provider } from "next-auth/client";
 import { IntlProvider } from "react-intl";
 
-// import SWRConfig from "../lib/swr-config";
 import NextAuth from "../lib/next-auth";
 import Messages from "../i18n/Messages";
 import "../styles/globals.css";
-
-console.log(process.env.PREVIEW_PASSWORD_HASH);
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -32,28 +29,6 @@ function MyApp({ Component, pageProps }) {
       </Provider>
     </>
   );
-  // return (
-  //   <>
-  //     <Provider session={pageProps.session}>
-  //       <IntlProvider
-  //         key={router.locale}
-  //         locale={router.locale}
-  //         messages={Messages[router.locale]}
-  //         defaultLocale="en-US"
-  //       >
-  //         <SWRConfig {...pageProps}>
-  //           {Component.auth ? (
-  //             <NextAuth>
-  //               <Component {...pageProps} />
-  //             </NextAuth>
-  //           ) : (
-  //             <Component {...pageProps} />
-  //           )}
-  //         </SWRConfig>
-  //       </IntlProvider>
-  //     </Provider>
-  //   </>
-  // );
 }
 
 export default MyApp;
